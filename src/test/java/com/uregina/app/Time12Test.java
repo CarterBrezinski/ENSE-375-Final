7ec88232118454d3d1f785495ffb9656b12e93f4
Unknown <Carter.Brezinski@gmail.com>
2021-04-27 15:10:22 -0600

    Remaking Time12Test

---
+++ src/test/java/com/uregina/app/Time12Test.java
@@ -0,0 +1,22 @@
+package com.uregina.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Time12Test 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void isTestRunning()
+    {
+        assertTrue( true );
+    }
+}
