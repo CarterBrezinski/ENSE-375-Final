18cb74ed0d41e21fd14e67695a1511c8f7dc0cb7
Unknown <Carter.Brezinski@gmail.com>
2021-04-27 14:49:23 -0600

    Removing error line in code

--- src/test/java/com/uregina/app/time12Test.java
+++ src/test/java/com/uregina/app/time12Test.java
@@ -15,7 +15,7 @@ public class Time12Test
     {
         AmPm pmInitiation = AmPm.pm;
         Time12 time12test = new Time12(5, 37, pmInitiation);
-        boolean validCase = ((time12test.getAm_or_PM()) == pmInitiation);
+        //boolean validCase = ((time12test.getAm_or_PM()) == pmInitiation);
         assertTrue(true);
     }
 
